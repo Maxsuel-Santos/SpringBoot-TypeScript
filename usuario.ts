59b09ce4f1d311d1af765bacca3db9470b895b86
Maxsuel-Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-08-08 15:22:10 -0300

    1 file modified

--- usuario.ts
+++ usuario.ts
@@ -3,9 +3,10 @@ import { deco01, deco02, deco03, deco04, deco05, deco06 } from './decorators';
 
 //deco06
 //@deco05
+//@deco04
 //@deco03({ a: 'Teste', b: 15 })
 //@deco02
-@deco01
+//@deco01
 export class Usuario {
 private nome: string;
 
